9484b04825172c0ff1c8be3a6a064f7cc66826f4
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-07-09 01:11:49 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-   <button class="btn btn-info">Info</button>
-<button class="btn btn-success">Success</button>
-<button class="btn btn-warning">Warning</button>
-<button class="btn btn-error">Error</button>
+   <Home/>
     </div>
   );
 }
