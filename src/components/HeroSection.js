6d8083ad9fd6bc0c9c2696307c55f9519311d300
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-07-13 19:32:09 +0600

    add

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroSection = () => {
     return (
         <div className='px-14'>
-            <div class="flex w-full gap-7">
+            <div class="flex w-full gap-9">
                 <div class="grid  flex-grow card bg-base-300 rounded-box place-items-center">
                     <img src="https://multikart-react.vercel.app/_next/static/images/sub-banner1-d89099e9c448c84a71f131fd01d931ec.jpg" height="100%" width="100%" alt="" />
                 </div>
